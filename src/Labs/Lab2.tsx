3899e875d2557c52d33f47de7ed62e9acaba65dd
nsankethreddy <sankethn1947@gmail.com>
2024-09-08 17:11:54 -0400

    followed a1

---
+++ src/Labs/Lab2.tsx
@@ -0,0 +1,18 @@
+export default function Lab1(){
+    return(
+        <div>
+            <h2>
+                Lab2
+            </h2>
+            <h3>
+                a1
+            </h3>
+            <h3>
+                a2
+            </h3>
+            <h3>
+                a3
+            </h3>
+        </div>
+    )
+}
---
+++ src/Labs/index.tsx
@@ -0,0 +1,12 @@
+import Lab1 from "./Lab1"
+import Lab2 from "./Lab2"
+export default function Labs(){
+    return(
+        <div>
+            <h1> Web-dev Labs </h1>
+            <Lab1 />
+            <Lab2 />
+            <h2> lab3 </h2>
+        </div>
+    )
+}
