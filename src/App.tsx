bd87074de695a147fd2db42228a64a4834485a60
nsankethreddy <sankethn1947@gmail.com>
2024-09-17 20:45:07 -0400

    Link is failing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import React from "react";
 import Labs from "./Labs";
+import Kanbas from "./Kanbas";
+import { HashRouter, Route, Routes, Navigate } from "react-router-dom";
 export default function App() {
   return (
-    <div>
-      <Labs />
-    </div>
+    <HashRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Kanbas" />} />
+          <Route path="/Labs/*" element={<Labs />} />
+          <Route path="/Kanbas/*" element={<Kanbas />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
-
-// export default App;
